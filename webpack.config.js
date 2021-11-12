4700135883527678de284cf7fd7a1d50365fb5ca
IZZAGOLD <bandurin.lawyer@gmail.com>
2021-11-12 16:13:57 +0400

    update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
     entry: {
-        main: path.resolve(__dirname, './src/pages/index/index.js'),
+        main: path.resolve(__dirname, './src/pages/Colors&Type/colors-type.js'),
     },
     output: {
         filename: '[name].bundle.js',
@@ -22,6 +22,7 @@ module.exports = {
                     pretty: true,
                 },
             },
+
             {
                 test: /\.(sa|sc|c)ss$/,
                 use: [
@@ -30,13 +31,41 @@ module.exports = {
                     'postcss-loader',
                 ],
             },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                exclude: [
+                    path.resolve(__dirname, './src/img/'),
+                    path.resolve(__dirname, './src/components/'),
+                ],
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'fonts/',
+                    },
+                }],
+            },
+            {
+                test: /\.(png|gif|svg|jpe?g)$/,
+                exclude: [
+                    path.resolve(__dirname, './src/fonts/'),
+                    path.resolve(__dirname, './src/favicons/'),
+                ],
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'img/',
+                    },
+                }],
+            },
         ],
     },
     plugins: [
         new MiniCssExtractPlugin(),
         new HtmlWebpackPlugin({
             title: 'title',
-            template: path.resolve(__dirname, './src/pages/index/index.pug'),
+            template: path.resolve(__dirname, './src/pages/Colors&Type/colors-type.pug'),
             filename: 'index.html',
         }),
     ],
