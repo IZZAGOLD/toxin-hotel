7267b3d7762a7f7cbcf1cbe0b88d361141596dd6
IZZAGOLD <bandurin.lawyer@gmail.com>
2021-11-25 11:36:56 +0400

    created сheckbox component; like-button component; radio-button component

---
+++ src/components/like-button/const.js
@@ -0,0 +1,11 @@
+const CLASS_FAVORITE = 'like-button_favorite';
+const SELECTOR_BUTTON = '.js-like-button';
+const SELECTOR_COUNT = '.js-like-button__counter';
+const SELECTOR_ICON = '.js-like-button__icon';
+
+export {
+    CLASS_FAVORITE,
+    SELECTOR_BUTTON,
+    SELECTOR_COUNT,
+    SELECTOR_ICON,
+};
---
+++ src/components/like-button/like-button.js
@@ -0,0 +1,35 @@
+import $ from 'jquery';
+import {
+    CLASS_FAVORITE,
+    SELECTOR_BUTTON,
+    SELECTOR_COUNT,
+    SELECTOR_ICON,
+} from './const';
+
+const LikeButton = function LikeButton() {
+    this.init();
+};
+
+LikeButton.prototype.init = function init() {
+    this.addEventListeners();
+};
+
+LikeButton.prototype.addEventListeners = function addEventListeners() {
+    $(SELECTOR_BUTTON).on('click', this.handleButtonClick.bind(this));
+};
+
+LikeButton.prototype.handleButtonClick = function handleButtonClick(e) {
+    e.preventDefault();
+
+    $(e.currentTarget).toggleClass(CLASS_FAVORITE);
+
+    const $counter = $(e.currentTarget).find(SELECTOR_COUNT);
+    const isFavorite = $(e.currentTarget).hasClass(CLASS_FAVORITE);
+
+    const count = parseInt($counter.text(), 10);
+    $counter.text(isFavorite ? count + 1 : count - 1);
+
+    $(e.currentTarget).find(SELECTOR_ICON).text(isFavorite ? 'favorite' : 'favorite_border');
+};
+
+$(() => new LikeButton());
