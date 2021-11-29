7bf22a9513e28fd71b13db62a2450dfa7481ce2b
IZZAGOLD <bandurin.lawyer@gmail.com>
2021-11-29 13:10:37 +0400

    created bullet-lis component; feedback component; pagination component; slider component

---
+++ src/components/checkbox-expandable-list/checkbox-expandable-list.js
@@ -0,0 +1,26 @@
+import $ from 'jquery';
+import {
+    CLASS_EXPANDED,
+    SELECTOR_TOGGLE,
+    SELECTOR_CHECKBOX_EXPANDABLE_LIST,
+} from './const';
+
+const CheckboxExpandableList = function CheckboxListExpandable() {
+    this.init();
+};
+
+CheckboxExpandableList.prototype.init = function init() {
+    this.addEventListeners();
+};
+
+CheckboxExpandableList.prototype.addEventListeners = function addEventListeners() {
+    $(SELECTOR_TOGGLE).on('click', this.handleToggleClick.bind(this));
+};
+
+CheckboxExpandableList.prototype.handleToggleClick = function handleToggleClick(e) {
+    e.preventDefault();
+
+    $(e.currentTarget).closest(SELECTOR_CHECKBOX_EXPANDABLE_LIST).toggleClass(CLASS_EXPANDED);
+};
+
+$(() => new CheckboxExpandableList());
---
+++ src/components/checkbox-expandable-list/const.js
@@ -0,0 +1,10 @@
+const CLASS_EXPANDED = 'checkbox-expandable-list_expanded';
+
+const SELECTOR_TOGGLE = '.js-checkbox-expandable-list__toggle-button';
+const SELECTOR_CHECKBOX_EXPANDABLE_LIST = '.js-checkbox-expandable-list';
+
+export {
+    CLASS_EXPANDED,
+    SELECTOR_TOGGLE,
+    SELECTOR_CHECKBOX_EXPANDABLE_LIST,
+};
