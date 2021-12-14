ef39b1587e88eec955259fe0263b5d5faf9c12ed
IZZAGOLD <bandurin.lawyer@gmail.com>
2021-12-14 15:46:18 +0400

    finished landing page

---
+++ src/pages/Landing/landing.js
@@ -0,0 +1,7 @@
+import $ from 'jquery';
+import  '../../utils/app.scss'
+import '../../components/header/header.scss'
+import '../../components/header/header'
+import '../../components/footer/footer.scss'
+import '../../components/room-search/room-search'
+import './landing.scss'
