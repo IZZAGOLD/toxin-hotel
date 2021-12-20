e62088d8787a4f4f8d026adf05dba1c5b030da23
IZZAGOLD <bandurin.lawyer@gmail.com>
2021-12-20 10:54:32 +0400

    finished registration page

---
+++ src/pages/Registration/registration.js
@@ -0,0 +1,7 @@
+import $ from 'jquery'
+import  '../../utils/app.scss'
+import '../../components/header/header.scss'
+import '../../components/header/header'
+import '../../components/footer/footer.scss'
+import '../../components/sign-up/sign-up'
+import './registration.scss'
--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ console.log(mode + ' mode')
 module.exports = {
     mode: mode,
     entry: {
-        scripts: path.resolve(__dirname, './src/pages/Room-details/room-details.js'),
+        scripts: path.resolve(__dirname, './src/pages/Registration/registration.js'),
         // user: './src/user.js',
     },
     output: {
@@ -36,7 +36,7 @@ module.exports = {
             filename: '[name].[contenthash].css'
         }),
         new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, './src/pages/Room-details/room-details.pug'),
+            template: path.resolve(__dirname, './src/pages/Registration/registration.pug'),
             filename: 'index.html',
         }),
         new webpack.ProvidePlugin({
@@ -101,5 +101,5 @@ module.exports = {
                 }
             }
         ]
-    },
+    }
 }
